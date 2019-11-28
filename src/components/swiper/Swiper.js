a87265d5538a46432dae67e305fc9b1f47b79962
Thalita Mariano <8.litah.8@gmail.com>
2019-11-28 17:40:01 -0300

    adicionando base do projeto

---
+++ src/components/swiper/Swiper.js
@@ -0,0 +1,67 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import morango from "../../assets/img/Slider/morango.png"
+import banana from "../../assets/img/Slider/banana.png"
+import abacaxi from "../../assets/img/Slider/abacaxi.png"
+import tomate from "../../assets/img/Slider/tomate.png"
+
+import 'swiper/css/swiper.css';
+
+// export default function Swiper(){
+//     return(
+//         <div>
+//             <div className="swiper-container sombra">
+//             <div className="back_swiper">
+//               <div className="swiper-container">
+//                 <div className="swiper-wrapper">
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={banana} alt="Imagem de bananas"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={morango} alt="Imagem de morangos"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={abacaxi} alt="Imagem de um abacaxi"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={tomate} alt="Imagem de tomates"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={banana} alt="Imagem de bananas"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={morango} alt="Imagem de morangos"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={abacaxi} alt="Imagem de um abacaxi"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+//                   <div className="swiper-slide">
+//                     <Link href=""><img src={tomate} alt="Imagem de tomates"/></Link>
+//                     <p>Cesta Sortidas</p>
+//                     <p>Por apenas R$1,00</p>
+//                   </div>
+                  
+//                   </div>
+//                 </div>
+//                 </div>
+                
+//                 <div className="swiper-pagination"></div>
+             
+//             </div>
+//         </div>
+//     )
+// }
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/estilo.css'
+//import "./assets/css/imagens/Slider/aaa.jpg"
+import App from './pages/home/App';
+import * as serviceWorker from './serviceWorker';
+
+import QuemSomos from "./pages/quemSomos/QuemSomos"
+// import Cadastro from "./pages/cadastro/Cadastro"
+// import PerfilComprador from "./pages/perfilComprador/PerfilComprador"
+// import BuscarProdutos from "./pages/buscarProdutos/BuscarProdutos"
+// import ProdutosEncontrados from "./pages/produtosEncontrados/ProdutosEncontrados"
+// import Receitas from "./pages/receitas/Receitas"
+// import PerfilProdutor from "./pages/perfilProdutor/PerfilProdutor"
+// import ProdutosCadastrados from "./pages/produtosCadastrados/ProdutosCadastrados"
+// import CadastrarProdutos from "./pages/cadastrarProdutos/CadastrarProdutos"
+
+import {Route, HashRouter as Router} from 'react-router-dom';
+
+const Rotas = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/QuemSomos" component={QuemSomos} />
+            {/* <Route path="/cadastro" component={Cadastro} />
+            <Route path="/PerfilComprador" component={PerfilComprador} />
+            <Route path="/PerfilProdutor" component={PerfilProdutor} />
+            <Route path="/BuscarProdutos" component={BuscarProdutos} />
+            <Route path="/ProdutosEncontrados" component={ProdutosEncontrados} />
+            <Route path="/Receitas" component={Receitas} />
+            <Route path="/ProdutosCadastrados" component={ProdutosCadastrados} />
+            <Route path="/CadastrarProdutos" component={CadastrarProdutos} /> */}
+            
+            
+        </div>
+    </Router>
+)
+
+ReactDOM.render(Rotas, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
