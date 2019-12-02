04724b78d9a6f946fb4e450409758755a510f6b9
Igor Calandrino <kingduffiy@gmail.com>
2019-12-02 16:03:00 -0300

    Login e Token
    Adicionado autenticação do Login e Token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './assets/css/estilo.css'
 //import "./assets/css/imagens/Slider/aaa.jpg"
 import App from './pages/home/App';
 import * as serviceWorker from './serviceWorker';
+import Login from "../src/components/header/Header"
 
 import QuemSomos from "./pages/quemSomos/QuemSomos"
 // import Cadastro from "./pages/cadastro/Cadastro"
@@ -15,22 +16,59 @@ import QuemSomos from "./pages/quemSomos/QuemSomos"
 // import ProdutosCadastrados from "./pages/produtosCadastrados/ProdutosCadastrados"
 // import CadastrarProdutos from "./pages/cadastrarProdutos/CadastrarProdutos"
 
-import {Route, HashRouter as Router} from 'react-router-dom';
+import {Route, HashRouter as Router, Switch, Redirect} from 'react-router-dom';
+import { usuarioAutenticado, parseJwt } from './services/auth';
+
+const PermissaoAdmin = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "3" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
+const PermissaoProdutor = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "2" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
+const PermissaoComprador = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "1" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
 
 const Rotas = (
     <Router>
         <div>
+            <Switch>
             <Route exact path="/" component={App} />
-            <Route path="/QuemSomos" component={QuemSomos} />
+            <PermissaoAdmin exact path="/quemsomos" component={QuemSomos} />
             {/* <Route path="/Cadastro" component={Cadastro} />
-            <Route path="/PerfilComprador" component={PerfilComprador} />
-            <Route path="/PerfilProdutor" component={PerfilProdutor} />
+            <PermissaoComprador path="/PerfilComprador" component={PerfilComprador} />
+            <PermissaoProdutor path="/PerfilProdutor" component={PerfilProdutor} />
             <Route path="/BuscarProdutos" component={BuscarProdutos} />
             <Route path="/ProdutosEncontrados" component={ProdutosEncontrados} />
             <Route path="/Receitas" component={Receitas} />
             <Route path="/ProdutosCadastrados" component={ProdutosCadastrados} />
             <Route path="/CadastrarProdutos" component={CadastrarProdutos} /> */}
-            
+            <Route exact path="/login" component={Login}/>
+            </Switch>
             
         </div>
     </Router>
