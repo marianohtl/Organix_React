fed57e747273671b1f290210a971636a18cb13b5
Aleixoslary <larissaaleixos@hotmail.com>
2019-12-05 11:08:13 -0300

    Perfil Comprador GET usuario pronto

--- src/pages/perfilComprador/PerfilComprador.js
+++ src/pages/perfilComprador/PerfilComprador.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import '../../assets/css/estilo.css';
 
@@ -11,136 +11,136 @@ import Footer from '../../components/Footer/Footer'
 import api from '../../services/api';
 
 
-// import { MDBBtn, MDBInput, MDBAlert, MDBContainer, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter} from "mdbreact";
-
-
-
-export default class PerfilComprador extends Component{
+export default class PerfilComprador extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             listaUsuario: [],
-            // listaTelefone:[],
+            listaTelefone: [],
+            listaEndereco: []
 
         }
     }
-    // getUsuario = () => {
-    //     api.get('/Usuario')
-    //         .then(response => {
-    //             if (response.status === 200) {
-    //                 this.setState({ listaUsuario: response.data })
-    //             }
-    //             console.log(response)
-    //         })
-    // }
 
     componentDidMount() {
         this.getUsuarioId();
-        // this.getTelefone();
-        // console.log(parseJwt().IdUsuario)
-
-        console.log(this.state.listaUsuario)
-     }
+        this.getTelefone();
+        this.getEndereco();
+    }
 
     getUsuarioId = () => {
-        
+
         let idUser = parseJwt().IdUsuario
-        console.log(idUser)
+        console.log('id de usuario logado', idUser)
         api.get('/Usuario/' + idUser)
-        .then(response => {
-            if (response.status === 200) 
-            this.setState({ listaUsuario: [response.data] })
-            console.log(this.state.listaUsuario)
-    
+            .then(response => {
+                if (response.status === 200)
+                    this.setState({ listaUsuario: [response.data] })
+                console.log(this.state.listaUsuario)
+
             })
     }
 
-    // getTelefone = () => {
-    //     // let idUser = parseJwt().IdUsuario
-    //     // console.log(idUser)
+    getTelefone = () => {
+        api.get('/Telefone')
+            .then(response => {
+                console.log('peguei os tel')
+                if (response.status === 200)
+                    this.setState({ listaTelefone: response.data })
+                console.log('lista de telefone', this.state.listaTelefone)
+            }
+        )
+    }
 
-    //     api.get('/Telefone')
-    //         .then(response => {
-    //             if (response.status === 200) 
-    //             this.setState({ listaTelefone: [response.data] })
-    //         })
-    // }
+    getEndereco = () => {
+        api.get('/Endereco')
+            .then(response => {
+                console.log('peguei os end')
+                if (response.status === 200)
+                    this.setState({ listaEndereco: response.data })
+                console.log('lista de Endereco', this.state.listaEndereco)
+            }
+        )
+    }
 
-
-    render(){
-        
-        return(
+    render() {
+        return (
             <div>
-            <main class="itens-encontrados">
-                <PainelAdm/>
-            <div class="lado-direito-perfil-produtor">
-
-                    <div class="container-perfil">
-        
-                        <h2>Meus Dados</h2>
-        
-                        <div class="container-perfil2">
-        
-                            <div class="dados-produtor">
-                            <h4>Dados Pessoais</h4>
+                <main class="itens-encontrados">
+                    <PainelAdm />
+                    <div class="lado-direito-perfil-produtor">
+                        <div class="container-perfil">
+                            <h2>Meus Dados</h2>
+                            <div class="container-perfil2">
                                 {
                                     this.state.listaUsuario.map(
-                                        function(u){
-                                            return(
-                                            <>
-                                                <p><span class="bold-info-type">Nome: </span> {u.nome}</p>
-                                                <p><span class="bold-info-type">CPF: </span>{u.cpfCnpj}</p>
-                                                {/* {
-                                                    this.u.telefone.map(
-                                                        function(t){
-                                                            return(
-                                                                 <p><span class="bold-info-type">Telefone:</span> {t.telefone1}</p>
+                                        function (u) {
+                                            return (
+                                                <div class="dados-produtor">
+                                                    <h4>Dados Pessoais</h4>
+                                                    <>
+                                                        <p><span class="bold-info-type">Nome: </span> {u.nome}</p>
+                                                        <p><span class="bold-info-type">CPF: </span>{u.cpfCnpj}</p>
+                                                        {
+                                                            this.state.listaTelefone.map(
+                                                                function (t) {
+                                                                    if (t.idUsuario === u.idUsuario) {
+                                                                        return (
+                                                                            <p><span class="bold-info-type">Telefone: </span> {t.telefone1}</p>
+                                                                        )
+                                                                    }
+                                                                }.bind(this)
                                                             )
-                                                        }.bind(this)
-                                                        )
-                                                }
-                                                */}
-                                                <p><span class="bold-info-type">E-mail: </span>{u.email}</p>
-                                            </>                                   
+                                                        }
+                                                        <p><span class="bold-info-type">E-mail: </span>{u.email}</p>
+                                                    </>
+                                                </div>
                                             )
                                         }.bind(this)
                                     )
                                 }
+                                <div class="dados-localizacao-produtor">
+                                    <h4>Endereço</h4>
+                                    {
+                                        this.state.listaUsuario.map(
+                                            function (u) {
+                                                return (
+                                                    <>
+                                                        {
+                                                            this.state.listaEndereco.map(
+                                                                function (e) {
+                                                                    if (e.idUsuario === u.idUsuario) {
+                                                                        return (
+                                                                            <>
+                                                                                <p><span class="bold-info-type">CEP: </span>{e.cep}</p>
+                                                                                <p><span class="bold-info-type">Endereco: </span>{e.rua}</p>
+                                                                                <p><span class="bold-info-type">Bairro: </span>{e.bairro}</p>
+                                                                                <p><span class="bold-info-type">Cidade: </span>{e.cidade}</p>
+                                                                                <p><span class="bold-info-type">Estado: </span>{e.estado}</p>
+                                                                                <p><span class="bold-info-type">Zona: </span>{e.regiao}</p>
+                                                                            </>
+                                                                        )
+                                                                    }
+                                                                }.bind(this)
+                                                            )
+                                                        }
+                                                    </>
+                                                )
+                                            }.bind(this)
+                                        )
+                                    }
+                                </div>
                             </div>
-        
-                            <div class="dados-localizacao-produtor">
-
-                                <h4>Endereço</h4>
-                                {
-                                    this.state.listaUsuario.map(
-                                        function(e){
-                                            return(
-                                                <>
-                                <p><span class="bold-info-type">CEP:</span></p>
-                                <p><span class="bold-info-type">Endereço:</span></p>
-                                <p><span class="bold-info-type">Bairro:</span> Algum</p>
-                                <p><span class="bold-info-type">Cidade:</span> Osasco</p> 
-                                <p><span class="bold-info-type">Estado:</span> SP</p>
-                                <p><span class="bold-info-type">Zona:</span> Zona Oeste</p>
-                            
-                            
-                            </>
-                            )
-                        }.bind(this)
-                    )
-                }
-                </div>
+                            <button class="editar-perfil" onClick="">Editar Perfil</button>
+                            <div class="lado-direito-resultado1"></div>
                         </div>
-                        <button class="editar-perfil" onClick="">Editar Perfil</button>
-                        <div class="lado-direito-resultado1"></div>
                     </div>
-                </div>
                 </main>
-                 <Footer/>
-                 </div>
-    ) 
-   
-}
+                <Footer />
+            </div>
+        )
+
+    }
 
 }
