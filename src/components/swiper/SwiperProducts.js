a8b812f417c8ebe2242cd7c0e616a86e3a8fb2e3
Marco F. Basile <uyguy66@hotmail.com>
2019-12-13 13:18:45 -0300

    Mudando minha branch

---
+++ src/components/swiper/SwiperProducts.js
@@ -0,0 +1,82 @@
+import React from "react"
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css';
+import { Component } from 'react'
+import '../../assets/css/estilo.css'
+import api from '../../services/api';
+
+
+export default class SwiperProducts extends Component {
+  constructor() {
+    super()
+    this.state = {
+      listaOferta: [],
+
+      getOferta: {
+        preco: "",
+        nomeProduto: "",
+      }
+    }
+    this.params = {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      observer: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    }
+  }
+
+  
+  getOferta = () => {
+    api.get('/Oferta')
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({ listaOferta: response.data }, () => console.log("produtos",this.state.listaOferta))
+        }
+      })
+  }
+
+
+  componentDidMount() {
+    this.getOferta();
+  }
+
+
+  render() {
+    return (
+      
+      // <Swiper {...SimpleSwiper.params}>
+      <Swiper {...this.params}>
+      
+      {this.state.listaOferta.map(function (a) {
+        return (
+                <div className="swiper-slide">
+                  <div className="card-produto">
+                    <div className="imagem-redonda-card-receita"> <img src="#"
+                      alt="torta de morango" /></div>
+                    <p className='nome-produto'>{a.nome_produto}</p>
+                    <ul>
+                      <li>Preço Médio: {a.preco}R$</li>
+                    </ul>
+                    <button type="button" to="/cadastro">VER PRODUTO</button>
+                  </div>
+                </div>
+                )
+              })}
+              
+        </Swiper>
+        )
+  }
+}
+
+
+
+/* // export default function Swiper(){
+//     return( */
+
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/estilo.css'
+//import "./assets/css/imagens/Slider/aaa.jpg"
+import App from './pages/home/App';
+import * as serviceWorker from './serviceWorker';
+import Login from "../src/components/header/Header"
+
+import QuemSomos from "./pages/quemSomos/QuemSomos"
+// import Cadastro from "./pages/cadastro/Cadastro"
+// import PerfilComprador from "./pages/perfilComprador/PerfilComprador"
+import BuscarProdutos from './pages/produtos/BuscarProdutos';
+import ProdutosEncontrados from "./pages/produtosEncontrados/ProdutosEncontrados"
+// import Receitas from "./pages/receitas/Receitas"
+import PerfilProdutor from "./pages/perfilProdutor/PerfilProdutor"
+// import ProdutosCadastrados from "./pages/produtosCadastrados/ProdutosCadastrados"
+// import CadastrarProdutos from "./pages/cadastrarProdutos/CadastrarProdutos"
+
+import {Route, HashRouter as Router, Switch, Redirect} from 'react-router-dom';
+import { usuarioAutenticado, parseJwt } from './services/auth';
+
+const PermissaoAdmin = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "3" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
+const PermissaoProdutor = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "2" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
+const PermissaoComprador = ({component : Component}) => (
+    <Route
+        render={props =>
+        usuarioAutenticado()  && parseJwt().Role === "1" ?(
+            <Component {...props}/>
+        ):(
+            <Redirect to={{pathname : "/login"}}/> 
+        )
+    }
+    />
+)
+
+const Rotas = (
+    <Router>
+        <div>
+            <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/quemsomos" component={QuemSomos} />
+            <Route path="/buscarprodutos" component={BuscarProdutos} />
+            <Route path="/PerfilProdutor" component={PerfilProdutor} />
+            <Route path="/ProdutosEncontrados" component={ProdutosEncontrados} />
+            {/* <Route path="/Cadastro" component={Cadastro} />
+            <PermissaoComprador path="/PerfilComprador" component={PerfilComprador} />
+            >
+            
+            <Route path="/Receitas" component={Receitas} />
+            <Route path="/ProdutosCadastrados" component={ProdutosCadastrados} />
+            <Route path="/CadastrarProdutos" component={CadastrarProdutos} /> */}
+            <Route path="/login" component={Login}/>
+            </Switch>
+            
+        </div>
+    </Router>
+)
+
+ReactDOM.render(Rotas, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
